64b0f95d9497b60faa7036ced864dccbe854295d
Sahand <sahandevs@gmail.com>
2019-04-09 15:55:55 +0430

    add test for InlineFragment

--- test/selection_test.dart
+++ test/selection_test.dart
@@ -28,7 +28,52 @@ void main() {
               .addDirective(IncludeDirective(Variable("var2")))
               .addDirective(IncludeDirective(Variable("var3")))
               .bake(),
-          equals("...frag1 @skip(if:\$var1) @include(if:\$var2) @include(if:\$var3)"));
+          equals(
+              "...frag1 @skip(if:\$var1) @include(if:\$var2) @include(if:\$var3)"));
+    });
+  });
+
+  group('test InlineFragment', () {
+    test('test bake method with simple parameter', () {
+      expect(InlineFragment().addSelection(Field("id")).bake(), "... {id}");
+      expect(
+          InlineFragment()
+              .addSelection(Field("id"))
+              .addSelection(Field("name"))
+              .bake(),
+          "... {id name}");
+    });
+    test('test bake method with directive', () {
+      expect(
+          InlineFragment()
+              .addSelection(Field("id"))
+              .addSelection(Field("name"))
+              .addDirective(IncludeDirective(Variable("var1")))
+              .bake(),
+          "... @include(if:\$var1) {id name}");
+          expect(
+          InlineFragment()
+              .addSelection(Field("id"))
+              .addDirective(IncludeDirective(Variable("var1")))
+              .addDirective(SkipDirective(Variable("var2")))
+              .bake(),
+          "... @include(if:\$var1) @skip(if:\$var2) {id}");
+    });
+    test('test bake method with type condition and directive', () {
+      expect(
+          InlineFragment(typeCondition: TypeCondition("Cat"))
+              .addSelection(Field("id"))
+              .addSelection(Field("name"))
+              .addDirective(IncludeDirective(Variable("var1")))
+              .bake(),
+          "... on Cat @include(if:\$var1) {id name}");
+          expect(
+          InlineFragment(typeCondition: TypeCondition("Dog"))
+              .addSelection(Field("id"))
+              .addDirective(IncludeDirective(Variable("var1")))
+              .addDirective(SkipDirective(Variable("var2")))
+              .bake(),
+          "... on Dog @include(if:\$var1) @skip(if:\$var2) {id}");
     });
   });
 }
