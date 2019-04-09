8074ecf444b02dcc1b7e579194fac0d21b64a4b7
Sahand <sahandevs@gmail.com>
2019-04-09 10:23:02 +0430

    init

---
+++ example/graphql_builder_example.dart
@@ -0,0 +1,6 @@
+import 'package:graphql_builder/graphql_builder.dart';
+
+main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/graphql_builder.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library graphql_builder;
+
+export 'src/graphql_builder_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
