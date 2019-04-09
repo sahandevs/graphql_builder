c7c22bfcfc6b7a3d41312acd9d2fe9bc0b3a2422
Sahand <sahandevs@gmail.com>
2019-04-09 16:43:48 +0430

    add VariableDefinition test

--- test/variable_test.dart
+++ test/variable_test.dart
@@ -2,7 +2,6 @@ import 'package:graphql_builder/graphql_builder.dart';
 import 'package:test/test.dart';
 
 void main() {
-
   group('test variables', () {
     test('test bake method', () {
       expect(Variable("testVariable").bake(), equals("\$testVariable"));
@@ -10,4 +9,20 @@ void main() {
     });
   });
 
+  group('test variable definition', () {
+    test('test variable without default value', () {
+      expect(VariableDefinition("id", VariableType("Int")).bake(),
+          equals("\$id:Int"));
+      expect(
+          VariableDefinition("id", VariableType("Int", nullable: false)).bake(),
+          equals("\$id:Int!"));
+    });
+    test('test variable with default value', () {
+      expect(
+          VariableDefinition("id", VariableType("Int"),
+                  defaultValue: NumberConst(10))
+              .bake(),
+          equals("\$id:Int=10"));
+    });
+  });
 }
